c66d8bffa48a1699b2212a2409231553b0ae6095
Chloe <144107013+Charen523@users.noreply.github.com>
2024-05-28 20:09:33 +0900

    [feat] poison bush에 닿을 때 데미지 주기

--- Assets/Scripts/Objects/PoisonBush.cs
+++ Assets/Scripts/Objects/PoisonBush.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PoisonBush : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int damage;
+    public float damageRate;
+
+    private List<IDamagable> things = new List<IDamagable>();
+
+    private IEnumerator DealDamage()
     {
-        
+        while (things.Count != 0)
+        {
+            for (int i = 0; i < things.Count; i++)
+            {
+                things[i].TakePhysicalDamage(damage);
+                yield return new WaitForSeconds(damageRate);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("들어옴");
+        if (other.gameObject.TryGetComponent(out IDamagable damagable))
+        {
+            Debug.Log("데미지 가능");
+            things.Add(damagable);
+            StartCoroutine(DealDamage());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out IDamagable damagable))
+        {
+            things.Remove(damagable);
+        }
     }
 }
